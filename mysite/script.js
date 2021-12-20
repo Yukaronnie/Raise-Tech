b977c1ff2555353594a1db3cec2a716092d97bb3
Yukaronnie <87079290+Yukaronnie@users.noreply.github.com>
2021-12-20 14:38:58 +0900

    Added a file of Old Raise Tech Website

---
+++ mysite/script.js
@@ -0,0 +1,22 @@
+/*$(function(){
+  $('.sp-submenu').hover(
+    function(){
+      $(this).find('.contents').addClass('contents-active');
+    },
+    function(){
+      $(this).find('.contents').removeClass('contents-active');
+    });
+    
+});
+$('.sp-submenu').hover(
+  function(){
+    var $contents=$(this).find('.contents');
+    $contents.slideDown();
+   
+
+  },
+  function(){
+    var $contents=$(this).find('.contents');
+    $contents.slideUp();
+  
+  });*/
